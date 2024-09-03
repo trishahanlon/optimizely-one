51ef8f7b5489efc339a2ecb7c87b6502d95533e6
Trisha Hanlon <tht8427@gmail.com>
2024-09-03 14:01:58 -0500

    Update routes.ts

--- app/api/me/[[...path]]/routes.ts
+++ app/api/me/[[...path]]/routes.ts
@@ -1,10 +1,10 @@
-import { createOptimizelyOneApi } from '@remkoj/optimizely-one-nextjs/server';
+import { createOptimizelyOneApi } from '@remkoj/optimizely-one-nextjs/api';
 
 const handler = createOptimizelyOneApi()
 
-export const GET = handler  
-export const POST = handler  
-export const runtime = 'edge' // 'nodejs' (default) | 'edge'  
-export const dynamic = 'force-dynamic'  
-export const dynamicParams = true  
+export const GET = handler
+export const POST = handler
+export const runtime = 'nodejs' // 'nodejs' (default) | 'edge'
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
 export const fetchCache = 'default-no-store'
